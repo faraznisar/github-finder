20b723119598389c0e58c6669d676f95a7efe6f8
Faraz Shah <faraz.nisar1@gmail.com>
2020-07-12 18:42:16 +0530

    Added env variables and github key

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
